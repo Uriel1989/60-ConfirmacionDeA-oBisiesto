0215da9abf1be1add7f81b337907837164433d03
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:22:32 -0300

    Add files via upload

---
+++ 60-ConfirmacionDeAñoBisiesto/ConfirmacionDeAñoBisiesto/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConfirmacionDeAñoBisiesto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorLeft = 30;
+
+            Console.WriteLine("Aqui determinaremos si un año es bisiesto o no, ingrese el año en cuestion:");
+            int año = Convert.ToInt32(Console.ReadLine());
+
+
+            while (año == 1700 || año == 1800 || año == 1900 || año == 1900 || año == 2100 || año == 2200 || año == 2300)
+            {
+               
+                Console.WriteLine("Año no valido, re-ingrese:");
+                año = Convert.ToInt32(Console.ReadLine());
+            }
+            if (año % 4 == 0)
+            {
+                Console.WriteLine("Es un año bisiesto");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
